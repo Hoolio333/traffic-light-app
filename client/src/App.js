87bf71f4d1759a1b5e5cac6dec46d5986e87c2f4
Chris Hyde <c_hyde333@hotmail.com>
2022-07-26 20:55:19 +0100

    Gender Interest shown

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,19 @@ import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import OnBoarding from "./pages/OnBoarding";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const App = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+
+  const AuthToken = cookies.AuthToken;
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/onboarding" element={<OnBoarding />} />
+        {AuthToken && <Route path="/dashboard" element={<Dashboard />} />}
+        {AuthToken && <Route path="/onboarding" element={<OnBoarding />} />}
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,50 @@
 import TinderCard from "react-tinder-card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ChatContainer from "../ChatContainer";
+import { useCookies } from "react-cookie";
+import axios from "axios";
 
 const Dashboard = () => {
+  const [user, setUser] = useState(null);
+  const [genderedUsers, setGenderedUsers] = useState(null);
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+
+  const userId = cookies.UserId;
+  const getUser = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/user", {
+        params: { userId },
+      });
+      setUser(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getGenderedUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/gendered-users", {
+        params: { gender: user?.gender_interest },
+      });
+      setGenderedUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      getGenderedUsers();
+    }
+  }, [user]);
+
+  console.log("user", user);
+  console.log("gendered users", genderedUsers);
+
   const characters = [
     {
       name: "Richard Hendricks",
@@ -39,7 +81,7 @@ const Dashboard = () => {
 
   return (
     <div className="dashboard">
-      <ChatContainer />
+      <ChatContainer user={user} />
       <div className="swipe-container">
         <div className="card-container">
           {characters.map((character) => (
